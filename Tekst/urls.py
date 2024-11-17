bce2cf6b635d5d4b64b7072536ecc0b2394fb67d
kaloydimitrov <kalotablet2006@gmail.com>
2024-11-17 17:44:03 +0300

    bulgarian title + removed media folder from gitignore

--- Tekst/urls.py
+++ Tekst/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import os
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,8 +28,5 @@ urlpatterns = [
     path('api/', include('api.urls')),
 ]
 
-if 'WEBSITE_HOSTNAME' in os.environ:
-    urlpatterns += staticfiles_urlpatterns()
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
