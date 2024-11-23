c2ce53386cf44ec840ae462a794e768a7a1513a3
kaloydimitrov <kalotablet2006@gmail.com>
2024-11-24 02:01:46 +0300

    many changes

--- routing/templates/includes/js/vue-component-comment.js
+++ routing/templates/includes/js/vue-component-comment.js
@@ -4,6 +4,9 @@ Vue.component('comment', {
     methods: {
         {% include 'includes/js/vue-method-convert-iso-8601.js' %}
         {% include 'includes/js/vue-method-adjust-textarea-height.js' %}
+        redirectToUser(username) {
+            window.location.href = `/user/profile/${username}/#report`;
+        },
         showCommentForm(commentId) {
             document.getElementById(`inputContainer${commentId}`).style.display = "block";
         },
@@ -147,7 +150,7 @@ Vue.component('comment', {
                 </div>
                 <div class="d-flex justify-content-between">
                     <small class="text-muted d-flex gap-1 align-items-center">
-                        <a class="d-flex align-items-end text-decoration-none" :href="'/user/profile/' + comment.user.slug">
+                        <a class="d-flex align-items-center text-decoration-none" :href="'/user/profile/' + comment.user.slug">
                             <svg xmlns="http://www.w3.org/2000/svg" height="19px" viewBox="0 -960 960 960" width="19px" class="icon-primary" fill="currentColor"><path d="M480-504.62q-49.5 0-84.75-35.25T360-624.62q0-49.5 35.25-84.75T480-744.62q49.5 0 84.75 35.25T600-624.62q0 49.5-35.25 84.75T480-504.62ZM200-215.38v-65.85q0-24.77 14.42-46.35 14.43-21.57 38.81-33.5 56.62-27.15 113.31-40.73 56.69-13.57 113.46-13.57 56.77 0 113.46 13.57 56.69 13.58 113.31 40.73 24.38 11.93 38.81 33.5Q760-306 760-281.23v65.85H200Zm40-40h480v-25.85q0-13.31-8.58-25-8.57-11.69-23.73-19.77-49.38-23.92-101.83-36.65-52.45-12.73-105.86-12.73t-105.86 12.73Q321.69-349.92 272.31-326q-15.16 8.08-23.73 19.77-8.58 11.69-8.58 25v25.85Zm240-289.24q33 0 56.5-23.5t23.5-56.5q0-33-23.5-56.5t-56.5-23.5q-33 0-56.5 23.5t-23.5 56.5q0 33 23.5 56.5t56.5 23.5Zm0-80Zm0 369.24Z"/></svg>
                             <span>[[ comment.user.username ]]</span>
                         </a>
@@ -163,7 +166,7 @@ Vue.component('comment', {
                             <li class="dropdown-item cursor-pointer" @click="deleteComment(comment)">Изтрий</li>
                         </ul>
                         <ul v-else class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <li class="dropdown-item cursor-pointer">Докладвай</li>
+                            <li class="dropdown-item cursor-pointer" @click="redirectToUser(comment.user.username)">Докладвай</li>
                         </ul>
                     </div>
                 </div>
